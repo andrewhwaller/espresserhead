21829fbefcb53b8acf4484a5735d8b762ba84c2a
Andrew Waller <48367637+andrewhwaller@users.noreply.github.com>
2019-05-22 23:41:46 -0500

    Add AR validations for coffee/user

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,4 +1,13 @@
 # Coffee Class
 class Coffee < ActiveRecord::Base
+
+  validates :name, :presence => true, length: { minimum: 5 }
+  validates :coffee_list_id, :presence => true, numericality: { only_integer: true }
+  validates :prep_method, :presence => true
+  validates :rating, :presence => true
+  validates :origin, :presence => true
+  validates :roast, :presence => true
+  validates :notes, length: { maximum: 500, too_long: "%{count} characters is the maximum allowed" }
+
   belongs_to :coffee_list
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,10 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :username, :presence => true,
-                       :uniqueness => true
+  validates :username, :presence => true, :uniqueness => true, length: { minimum: 5 }
   validates :email,    :presence => true,
                        :uniqueness => true
-  validates :password, :presence => true
+  validates :password, :presence => true, length: { in: 6..20 }
 
   has_many :coffee_lists
   has_many :coffees, through: :coffee_lists
